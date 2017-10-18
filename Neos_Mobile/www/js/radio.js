5a56ef8f21cd46afc1041ab6e286db1c1e0ec02a
youssefmadi <youssef-madi@hotmail.com>
2017-10-18 11:26:06 +0300

    Add files via upload

--- Neos_Mobile/www/js/radio.js
+++ Neos_Mobile/www/js/radio.js
@@ -1,10 +1,13 @@
 function prepareWorldRadio(){
     for (i = 0; i < 1; i++) {
-        $("#content").append("<div class='row'><div class='col-lg-12 col-md-12 col-sm-12 col-xs-12'><button id='play'>Play</button></div></div>");
+        //$("#content").append("<div class='row'><div class='col-lg-12 col-md-12 col-sm-12 col-xs-12'><button id='play'>Play</button></div></div>");
     }
-    var player = new rPlayer();
+    $("#content").append("<audio controls autoplay><source src='http://radio90live.go.ro:8000/listen.opus' type='audio/ogg;codecs=\"opus\"'>Your browser does not support the audio tag.</audio>");
+    
 
-    player.play(world_radio[0].path);
+    //var player = new rPlayer();
+    //alert(world_radio[0].path);
+   // player.play(world_radio[0].path);
     
     //console.log('Playing : ' + player.playing);
     //console.log('Volume : ' + player.volume);
