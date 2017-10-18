117d902be96ae9c90ec85d62d64c96f0d5e24797
youssefmadi <youssef-madi@hotmail.com>
2017-10-18 12:10:22 +0300

    Add files via upload

--- Neos_Mobile/www/js/radio.js
+++ Neos_Mobile/www/js/radio.js
@@ -1,10 +1,12 @@
 function prepareWorldRadio(){
-    for (i = 0; i < 1; i++) {
-        //$("#content").append("<div class='row'><div class='col-lg-12 col-md-12 col-sm-12 col-xs-12'><button id='play'>Play</button></div></div>");
+    for (i = 0; i < world_radio.length; i++) {
+        $("#content").append("<div class='col-lg-12 col-md-12 col-sm-12 col-xs-12'><div class='row'><div class='col-lg-6 col-md-6 col-sm-12 col-xs-12 radio_title'>"+world_radio[i].title+"</div><div class='col-lg-6 col-md-6 col-sm-12 col-xs-12'><audio id='r_"+world_radio[i].id+"' controls><source src='"+world_radio[i].path+"' type='audio/ogg;codecs=\"opus\"'>Your browser does not support the audio tag.</audio></div></div></div>");
     }
-    $("#content").append("<audio controls autoplay><source src='http://radio90live.go.ro:8000/listen.opus' type='audio/ogg;codecs=\"opus\"'>Your browser does not support the audio tag.</audio>");
+    setTimeout(function(){
+        var vid = document.getElementById("r_1"); 
+        vid.play(); 
+    },1000);
     
-
     //var player = new rPlayer();
     //alert(world_radio[0].path);
    // player.play(world_radio[0].path);
